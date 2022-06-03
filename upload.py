524064590676ed0f88f85627a0fce5ec11558a66
bending456 <bending456@gmail.com>
2022-06-03 14:54:23 -0400

    correction

--- upload.py
+++ upload.py
@@ -1,16 +1,16 @@
 import streamlit as st
 
 def main():
-	st.title("File Upload Tutorial")
+    st.title("File Upload Tutorial")
 
-	menu = ["Image","Dataset","DocumentFiles","About"]
-	choice = st.sidebar.selectbox("Menu",menu)
+    menu = ["Image","Dataset","DocumentFiles","About"]
+    choice = st.sidebar.selectbox("Menu",menu)
 
-	if choice == "Image":
-		st.subheader("Image")
+    if choice == "Image":
+        st.subheader("Image")
 
     elif choice == "Dataset":
-		st.subheader("Dataset")
+        st.subheader("Dataset")
 
     elif choice == "DocumentFiles":
-		st.subheader("DocumentFiles")
+        st.subheader("DocumentFiles")
